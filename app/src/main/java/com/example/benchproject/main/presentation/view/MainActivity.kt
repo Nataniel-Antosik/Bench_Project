47e8cb0d3769213d635730c6192defe07c15e199
nataniel.antosik <nataniel.antosik@intive.com>
2022-06-03 15:57:18 +0200

    Hilt Dependencies -> Setup Hilt

--- app/src/main/java/com/example/benchproject/main/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/benchproject/main/presentation/view/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.benchproject
+package com.example.benchproject.main.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.benchproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
