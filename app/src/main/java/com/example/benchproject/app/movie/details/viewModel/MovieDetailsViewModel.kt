9f7fabf0b492a3f8e3114db5d05fad729b620a5f
nataniel.antosik <nataniel.antosik@intive.com>
2022-07-04 11:00:16 +0200

    Movie details -> Add navigator to movie details

--- app/src/main/java/com/example/benchproject/app/movie/details/viewModel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/benchproject/app/movie/details/viewModel/MovieDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.benchproject.app.movie.details.viewModel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
@@ -8,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.benchproject.app.movie.details.entity.MovieDetails
 import com.example.benchproject.app.movie.details.view.MovieDetailsFragmentArgs
+import com.example.benchproject.app.movie.details.view.MovieDetailsFragmentNavigator
 import com.example.benchproject.domain.movie.details.usecase.GetMovieDetailsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,6 +17,7 @@ import javax.inject.Inject
 class MovieDetailsViewModel @Inject constructor(
     savedState: SavedStateHandle,
     private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
+    private val movieDetailsFragmentNavigator: MovieDetailsFragmentNavigator
 ) : ViewModel() {
 
     val responseMovieDetails: LiveData<MovieDetails>
@@ -31,8 +32,7 @@ class MovieDetailsViewModel @Inject constructor(
                 _responseMovieDetails.value = movieDetailsModel.toUi()
             },
             onFailure = {
-                // TODO changed it when navigator will be implemented
-                Log.d("RESPONSE", "Something went wrong")
+                movieDetailsFragmentNavigator.errorSnackBar("Something went wrong")
             }
         )
     }
