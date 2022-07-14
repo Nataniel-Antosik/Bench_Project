70b28db6b707fba27f063c27ff195966c050c8c4
nataniel.antosik <nataniel.antosik@intive.com>
2022-07-14 14:33:26 +0200

    Make modules APP DATA DOMAIN -> Move appropriate tests from app to data

--- app/src/test/java/com/antosik/benchproject/test/common/LiveDataTest.kt
+++ app/src/test/java/com/antosik/benchproject/test/common/LiveDataTest.kt
@@ -3,12 +3,14 @@ package com.antosik.benchproject.test.common
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.arch.core.executor.TaskExecutor
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class LiveDataTest : BeforeEachCallback, AfterEachCallback {
 
     override fun beforeEach(context: ExtensionContext?) {
