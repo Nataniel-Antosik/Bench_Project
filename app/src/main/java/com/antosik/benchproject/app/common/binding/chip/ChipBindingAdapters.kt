17325d51e2e7e87bc41a9413aa7b0b03c15c5401
nataniel.antosik <nataniel.antosik@intive.com>
2022-07-22 13:51:41 +0200

    Kotlinish -> Change genresChips adapter to more kotlinish

--- app/src/main/java/com/antosik/benchproject/app/common/binding/chip/ChipBindingAdapters.kt
+++ app/src/main/java/com/antosik/benchproject/app/common/binding/chip/ChipBindingAdapters.kt
@@ -7,11 +7,12 @@ import com.google.android.material.chip.ChipGroup
 
 @BindingAdapter("genresChips")
 fun ChipGroup.addGenresToChips(genres: List<Genres>?) {
-    if (genres != null) {
-        for (i in genres) {
-            val chip = Chip(context)
-            chip.text = i.name
-            addView(chip)
+    if (genres == null) return
+    genres.forEach {
+        Chip(context).apply {
+            text = it.name
+        }.also {
+            addView(it)
         }
     }
 }
