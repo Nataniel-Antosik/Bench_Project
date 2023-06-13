d36aba8be93a217d053c3d01737dbcaed83fdf89
nataniel.antosik <nataniel.antosik@intive.com>
2023-06-13 14:24:58 +0200

    After Borys Code Review -> delete redundant thing, replace bracket with expression body, drop redundant type

--- app/src/main/java/com/antosik/benchproject/app/common/recyclerView/RecyclerBaseAdapter.kt
+++ app/src/main/java/com/antosik/benchproject/app/common/recyclerView/RecyclerBaseAdapter.kt
@@ -2,14 +2,11 @@ package com.antosik.benchproject.app.common.recyclerView
 
 import android.annotation.SuppressLint
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class RecyclerBaseAdapter<T> : RecyclerView.Adapter<BaseViewHolder<T>>() {
 
     private var data = emptyList<T>()
-
-    abstract fun setViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T>
 
     @SuppressLint("NotifyDataSetChanged")
     fun uploadData(newData: List<T>) {
@@ -17,15 +14,12 @@ abstract class RecyclerBaseAdapter<T> : RecyclerView.Adapter<BaseViewHolder<T>>(
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> {
-        return setViewHolder(parent, viewType)
-    }
-
     override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
         holder.bind(data[position])
     }
 
-    override fun getItemCount(): Int = data.size
+    override fun getItemCount() =
+        data.size
 }
 
 abstract class BaseViewHolder<T>(view: View) : RecyclerView.ViewHolder(view) {
