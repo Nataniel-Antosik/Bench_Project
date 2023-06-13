1526dbe80df42c56f1c074538f120e57ebe58b3c
nataniel.antosik <nataniel.antosik@intive.com>
2023-06-13 15:32:58 +0200

    After Borys Code Review -> Not needed, do not inherit from NavigableFragment when navigation is not needed

--- app/src/main/java/com/antosik/benchproject/app/movie/details/view/MovieDetailsFragment.kt
+++ app/src/main/java/com/antosik/benchproject/app/movie/details/view/MovieDetailsFragment.kt
@@ -3,14 +3,14 @@ package com.antosik.benchproject.app.movie.details.view
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.antosik.benchproject.app.common.navigator.NavigableFragment
 import com.antosik.benchproject.app.movie.details.viewModel.MovieDetailsViewModel
 import com.antosik.benchproject.databinding.MovieDetailsFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MovieDetailsFragment : NavigableFragment<MovieDetailsFragmentNavigator>() {
+class MovieDetailsFragment : Fragment() {
 
     private val movieDetailsViewModel: MovieDetailsViewModel by viewModels()
 
