5c452630d091c133def582e91054442070799594
nataniel.antosik <nataniel.antosik@intive.com>
2023-07-18 09:26:29 +0200

    Add functionality for favorite movies -> Scheme to change color if movie is favorite

--- app/src/main/kotlin/com/antosik/benchproject/app/popular/movies/entity/Movie.kt
+++ app/src/main/kotlin/com/antosik/benchproject/app/popular/movies/entity/Movie.kt
@@ -1,5 +1,6 @@
 package com.antosik.benchproject.app.popular.movies.entity
 
+import com.antosik.benchproject.R
 import com.antosik.benchproject.domain.popular.movies.entity.MovieModel
 
 data class Movie(
@@ -9,6 +10,21 @@ data class Movie(
     val releaseDate: String,
     val imageUrl: String,
     val isFavorite: Boolean,
+    val colorFavorite: Int,
 )
 
-fun List<MovieModel>.toUi() = map { Movie(it.id, it.name, it.rating, it.releaseDate, it.imageUrl, it.isFavorite) }
+fun List<MovieModel>.toUi() = map {
+    Movie(
+        it.id,
+        it.name,
+        it.rating,
+        it.releaseDate,
+        it.imageUrl,
+        it.isFavorite,
+        it.isFavorite.changeColorIfFavorite(),
+    )
+}
+
+private fun Boolean.changeColorIfFavorite() =
+    if (this) R.color.red else R.color.black
+
